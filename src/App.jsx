ed89c258fcc3865587e64554fa8beb757e8647a5
whodeygus <gustin.puckett@gmail.com>
2025-07-31 00:21:15 -0400

    Admin Routing Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import RestaurantsPage from './components/RestaurantsPage';
