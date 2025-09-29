2084df6e04f89d6e239872f203bfc2997a6490e9
whodeygus <gustin.puckett@gmail.com>
2025-09-29 00:04:56 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import SimpleApp from './SimpleApp'
 import './index.css'
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: 'center', padding: '2rem' }}>
+        TENNsational - Site Under Maintenance
+      </h1>
+      <p style={{ textAlign: 'center' }}>
+        We'll be back online shortly. For inquiries: Admin@tennsational.com
+      </p>
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SimpleApp />
+    <App />
   </React.StrictMode>,
 )
