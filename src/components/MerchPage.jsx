fa140b960372fa6d540eebd305f65eadcfc561dc
whodeygus <gustin.puckett@gmail.com>
2025-10-01 00:10:53 -0400

    Update MerchPage.jsx

--- src/components/MerchPage.jsx
+++ src/components/MerchPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 
 const MerchPage = () => {
   return (
