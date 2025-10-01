444f59220affb6d1d5739aa29d041c9f66618420
whodeygus <gustin.puckett@gmail.com>
2025-10-01 00:53:43 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-
-function App() {
-  return (
-    <div>
-      <h1 style={{ textAlign: 'center', padding: '2rem' }}>
-        TENNsational - Site Under Maintenance
-      </h1>
-      <p style={{ textAlign: 'center' }}>
-        We'll be back online shortly. For inquiries: Admin@tennsational.com
-      </p>
-    </div>
-  );
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // Import your main App component
+import './index.css'; // Assuming you have a global CSS file
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
